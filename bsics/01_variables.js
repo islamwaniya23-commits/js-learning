b0cabe68a4a1f95c33e72dc9520ff590b00c4cec
islamwaniya23-commits <islamwaniya23@gmail.com>
2026-09-13 14:52:35 +0000

    add variables and datatypes

---
+++ bsics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountemail = "waniya@google.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+
+let accountstate;
+
+//accountId = 2 not allowed 
+accountemail = "wani@c.com"
+accountPassword="2121"
+accountCity="delhi"
+
+console.log(accountId);
+console.log(accountemail);
+
+console.table([accountId, accountemail, accountPassword, accountCity, accountstate])
---
+++ bsics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+//there is no mode for use no strict once declared uses the whole code as newer version
+
+//alert("hello") // arithmetic bhi use krte ye tb kr skte jb html file use krre ho bhi ham node m hai
+//alag syntac h node ke lie
+
+console.log(3+3); console.log("waniy") // YE Theek h but readibility ni hai
+
+// DATA TYPES
+let name = "string" // strings used both for single or double quotes
+let age = 18 // number 2 TO POWER 53
+let islogin = true // boolean either true or false
+//bigInt for very big numbers for trading websites
+// null => is actually a stand alone value(empty string representation)
+// undefined => when value of a variable is not defined
+// symbol => unique ke lie jb baat krte
+
+
+//object
+
+console.log(typeof age); // wille tell data type
+
+console.log(typeof null); // gives object as ans  
+console.log( typeof undefined) // give undefined as datatype
